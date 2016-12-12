b73c32203c31130b9980f8b6516725e958759603
Nguyen Hoa <nhoa@tma.com.vn>
2016-12-12 20:44:41 +0700

    MT-v2.9: Update admin site: login form

--- protected/modules/shop/views/contact/create.php
+++ protected/modules/shop/views/contact/create.php
@@ -16,7 +16,7 @@ $this->menu=array(
 <div class="row inner-bottom-sm">
 	<div class="contact-page">
 		<div class="col-md-12 contact-map outer-bottom-vs">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1958.4544782202067!2d106.4897454109576!3d10.970244437171392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5a40c0dee1443c4d!2zTmfDom4gSMOgbmcgQ8O0bmcgVGjGsMahbmcgVmnhu4d0IE5hbSAoVmlldGluYmFuaykgLSBDbiBD4bunIENoaQ!5e0!3m2!1svi!2s!4v1474784910038" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.856788281344!2d106.49434501480252!3d10.974180492187486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310b2b7d77c4f3ef%3A0x5cc600a066893181!2zQ-G7rWEgSMOgbmcgVsSDbiBQaMOybmcgUGjhuqltIE1pbmggVMO6!5e0!3m2!1sen!2s!4v1481510373088" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 		</div>
 		<h3>Nhập thông tin liên hệ</h3>
 		<hr>
